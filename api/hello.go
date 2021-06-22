70361b5af9692facf9ea453e74a30f265371f8c0
xiaoyuan.wang <xiaoyuan.wang@shopee.com>
2021-06-22 10:37:48 +0800

    add start client

--- api/hello.go
+++ api/hello.go
@@ -3,12 +3,18 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"math/rand"
 	"monitoring-entry-task/common"
 	"net/http"
+	"time"
 )
 func Hello(ctx *gin.Context) {
 	name := ctx.Query("name")
 	path := ctx.Request.URL.Path
+	//for test
+	n := rand.Intn(100)
+	time.Sleep((time.Duration)(n) * time.Millisecond)
+
 	if name == "" {
 		ctx.JSON(http.StatusOK, gin.H{
 			"status":  http.StatusNoContent,
--- main.go
+++ main.go
@@ -1,9 +1,61 @@
 package main
 
 import (
+	"log"
+	"math/rand"
 	"monitoring-entry-task/router"
+	"net/http"
+	"time"
 )
 
 func main()  {
+	go startClient()
 	router.InitRouter()
+}
+
+func startClient() {
+	sleepTime := 1000
+
+	go func() {
+		ticker := time.NewTicker(2 * time.Minute)
+		for {
+			<-ticker.C
+			sleepTime = 200
+			<-time.After(30 * time.Second)
+			sleepTime = 1000
+		}
+	}()
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for {
+				n := rand.Intn(100)
+				if n >= 95 {
+					sendErrRequest()
+				} else {
+					sendRequest()
+				}
+
+				time.Sleep((time.Duration)(sleepTime) * time.Millisecond)
+			}
+		}()
+	}
+}
+
+func sendRequest() {
+	resp, err := http.Get("http://localhost:9094/hello?name=tom")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	resp.Body.Close()
+}
+
+func sendErrRequest() {
+	resp, err := http.Get("http://localhost:9094/hello")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	resp.Body.Close()
 }
